5b389bd70f3a233b105d1e9969aa8191a1df6c66
Lucas Kreuch <lucaskreuch77@gmail.com>
2022-07-29 17:44:51 -0300

    Colisao

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js";
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from "./snake.js";
 import { update as updateFood, draw as drawFood} from "./food.js";
+import { outsideGrid } from "./grid.js";
 
-
+let gameOver = false;
 let lastRendertime = 0;
 const gameBoard = document.getElementById("game-board");
 
 
 function main(currentTime) {
+    if (gameOver) {
+        if (confirm("YOU LOSE. PRESS 'OK' TO RESTART.")) {
+            window.location = "/";
+        }
+        return
+    }
+
+
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRendertime) / 1000; // É dividido por 1000 para converter de milésimos para segundos
 
@@ -25,10 +34,15 @@ window.requestAnimationFrame(main)
 function update() {
     updateSnake();
     updateFood();
+    checkDeath();
 }
 
 function draw() {
     gameBoard.innerHTML = ""; // Limpa a tela
     drawSnake(gameBoard);
     drawFood(gameBoard);
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
--- js/snake.js
+++ js/snake.js
@@ -31,20 +31,29 @@ export function expandSnake(amount) {
     newSegments += amount;
 }
 
-export function onSnake(position) {
-    return snakeBody.some(segment => {
+export function onSnake(position, { ignoredHead = false } = {}) {
+    return snakeBody.some((segment, index) => {
+        if (ignoredHead && index === 0) return false;
         return equalPosition(segment, position);
     });
-}
+};
+
+export function getSnakeHead() {
+    return snakeBody[0];
+};
+
+export function snakeIntersection() {
+    return onSnake(snakeBody[0], { ignoredHead: true });
+};
 
 function equalPosition(pos1, pos2) {
     return pos1.x === pos2.x && pos1.y === pos2.y;
-}
+};
 
 function addSegments() {
     for (let i=0; i<newSegments; i++) {
         snakeBody.push({ ...snakeBody[snakeBody.length -1 ] });
-    }
+    };
 
     newSegments = 0;
-}
+};
